aa8004e15a3d616ebae9dc85666726e28ea9c385
Andrei Buinovskii <andre15101993@gmail.com>
2021-02-10 10:20:57 +0300

    [ex2-34] Установить значение тега meta «specialdate» в компоненте news.list

---
+++ local/templates/furniture_blue/components/bitrix/news/.default/.parameters.php
@@ -0,0 +1,13 @@
+<?
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+$arTemplateParameters = array(
+	"SPECIALDATE" => Array(
+		"NAME" => GetMessage("SPECIALDATE"),
+		"TYPE" => "CHECKBOX",
+		"MULTIPLE" => "N",
+		"VALUE" => "Y",
+		"DEFAULT" =>"N",
+	),
+);
+?>
